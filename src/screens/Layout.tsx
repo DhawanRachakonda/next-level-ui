ace9c67b6b93727c5c8a72d456ed3122ba6bd8f1
DhawanRachakonda <r.dhawan27@hotmail.co.uk>
2020-10-19 12:13:48 +0530

    feat: slider(desktop)

--- src/screens/Layout.tsx
+++ src/screens/Layout.tsx
@@ -10,7 +10,7 @@ import Button from '@material-ui/core/Button';
 
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 
 import NonSecureHeader from './headers/NonSecureHeader';
 
@@ -18,6 +18,16 @@ import NonSecureHeader from './headers/NonSecureHeader';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
+
+// Testimonial Slides
+import Badge from '@material-ui/core/Badge';
+import Avatar from '@material-ui/core/Avatar';
+import FormatQuoteIcon from '@material-ui/icons/FormatQuote';
+import ArrowLeftIcon from '@material-ui/icons/ArrowLeft';
+import ArrowRightIcon from '@material-ui/icons/ArrowRight';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import Slide from '@material-ui/core/Slide';
+import Paper from '@material-ui/core/Paper';
 
 // Footer
 import Divider from '@material-ui/core/Divider';
@@ -40,10 +50,15 @@ const useStyles = makeStyles((theme) => ({
     alignItems: 'center',
     borderRadius: '3vw',
     backgroundColor: '#CCF5FC',
+    background:
+      'linear-gradient(180deg, rgba(204,245,252,1) 0%, rgba(204,245,252,1) 30%, rgba(255,255,255,1) 100%)',
     padding: `${theme.spacing(1)}px 0rem`,
     marginTop: theme.spacing(3),
     [theme.breakpoints.up('sm')]: {
       padding: theme.spacing(5),
+    },
+    [theme.breakpoints.up('lg')]: {
+      padding: theme.spacing(12),
     },
   },
   homeSiteDescTypographyRow1: {
@@ -248,16 +263,27 @@ function TopProfessorsCourses() {
   );
 }
 
-const useTestimonialsStyles = makeStyles((theme) => ({
-  testimonialsContainerRoot: {
-    margin: `0em ${theme.spacing(0)}px`,
-    marginTop: theme.spacing(5),
+const NonOutlinedCard = withStyles((theme) => ({
+  root: {
+    boxShadow: 'unset',
   },
+}))(Card);
 
+const TestimonialGridContainer = withStyles((theme) => ({
+  root: {
+    margin: `${theme.spacing(5)}px 0px`,
+    [theme.breakpoints.up('lg')]: {
+      padding: theme.spacing(7),
+      paddingTop: 'unset',
+    },
+  },
+}))(Grid);
+
+const useTestimonialsStyles = makeStyles((theme) => ({
   testimonialsTitleCardContainer: {
     padding: theme.spacing(5),
+    paddingTop: 'unset',
     textAlign: 'left',
-    boxShadow: 'unset',
   },
   testimonialsTitleCardContentRoot: {
     paddingLeft: '0px',
@@ -275,6 +301,10 @@ const useTestimonialsStyles = makeStyles((theme) => ({
   },
   testimonialCardRow2: {
     marginTop: theme.spacing(5),
+    [theme.breakpoints.up('lg')]: {
+      lineHeight: `${theme.spacing(8)}px`,
+      width: theme.spacing(30),
+    },
   },
   testimonialCardRow3: {
     fontWeight: 100,
@@ -282,16 +312,20 @@ const useTestimonialsStyles = makeStyles((theme) => ({
   },
 }));
 
+const TestimonialGrid = withStyles((theme) => ({
+  root: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+}))(Grid);
+
 function Testimonials() {
   const styles = useTestimonialsStyles();
 
   return (
-    <Grid
-      classes={{ root: styles.testimonialsContainerRoot }}
-      item={true}
-      xs={12}>
-      <Grid item={true} xs={12} md={8} lg={12}>
-        <Card
+    <TestimonialGridContainer container item={true} xs={12}>
+      <Grid item={true} xs={12} lg={4}>
+        <NonOutlinedCard
           className={styles.testimonialsTitleCardContainer}
           classes={{ root: styles.testimonialsTitleCardContentRoot }}>
           <CardContent>
@@ -320,14 +354,22 @@ function Testimonials() {
               Read More Testimonials
             </Button>
           </CardActions>
-        </Card>
+        </NonOutlinedCard>
       </Grid>
-    </Grid>
+      <TestimonialGrid container={true} item={true} xs={12} md={4} lg={8}>
+        <TestimonySlides />
+      </TestimonialGrid>
+    </TestimonialGridContainer>
   );
 }
 
 const useFooterStyles = makeStyles((theme) => ({
-  footerContainerRoot: {},
+  footerContainerRoot: {
+    [theme.breakpoints.up('lg')]: {
+      padding: theme.spacing(7),
+      paddingTop: 'unset',
+    },
+  },
   logoContainerRoot: {
     padding: '0.5em 0.5em',
     paddingLeft: theme.spacing(0),
@@ -419,6 +461,249 @@ export function Footer() {
         </Grid>
       </Grid>
     </Grid>
+  );
+}
+
+const SliderCard = withStyles((theme) => ({
+  root: {
+    textAlign: 'center',
+    borderRadius: '3em',
+    margin: '0em 2em',
+    [theme.breakpoints.only('lg')]: {
+      width: `${theme.spacing(35)}px`,
+    },
+    [theme.breakpoints.up('xl')]: {
+      width: `${theme.spacing(42)}px`,
+    },
+  },
+}))(Card);
+
+const SmallAvatar = withStyles((theme) => ({
+  root: {
+    width: 40,
+    height: 40,
+    border: `2px solid #18A4E0`,
+    borderRadius: '50%',
+    backgroundColor: '#18A4E0',
+    color: '#F1F1F1',
+  },
+}))(FormatQuoteIcon);
+
+const StyledAvatar = withStyles((theme) => ({
+  root: {
+    width: '7em',
+    height: '6em',
+  },
+}))(Avatar);
+
+const TestimonialSliderCardName = withStyles((theme) => ({
+  root: {
+    fontWeight: 100,
+    marginTop: theme.spacing(1),
+  },
+}))(Typography);
+
+const Designation = withStyles((theme) => ({
+  root: {
+    fontWeight: 'bold',
+    marginTop: theme.spacing(0.2),
+    color: '#18A4E0',
+  },
+}))(Typography);
+
+interface ITestimonySlideProps {
+  item: {
+    id: number;
+    name: string;
+    description: string;
+    designation: string;
+  };
+}
+
+const LeftTypography = withStyles((theme) => ({
+  root: {
+    textAlign: 'left',
+  },
+}))(Typography);
+
+const SliderCardDescription = withStyles((theme) => ({
+  root: {
+    padding: `${theme.spacing(4)}px`,
+  },
+}))(LeftTypography);
+
+const SliderCardDescriptionDivider = withStyles((theme) => ({
+  root: {
+    margin: `0px ${theme.spacing(10)}px`,
+    backgroundColor: '#18A4E0',
+  },
+  variant: {
+    textAlign: 'center',
+  },
+}))(Divider);
+
+function TestimonySlide({
+  item: { id, name, description, designation },
+}: ITestimonySlideProps) {
+  return (
+    <React.Fragment>
+      <SliderCard>
+        <CardContent>
+          <Badge
+            overlap="circle"
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'right',
+            }}
+            badgeContent={<SmallAvatar />}>
+            <StyledAvatar
+              alt="Travis Howard"
+              src={`/assets/images/Ellipse_${id / 2 === 0 ? '6' : '7'}.png`}
+            />
+          </Badge>
+          <SliderCardDescription variant="body2">
+            {description}
+          </SliderCardDescription>
+          <SliderCardDescriptionDivider />
+          <TestimonialSliderCardName variant="subtitle1">
+            {name}
+          </TestimonialSliderCardName>
+          <Designation variant="subtitle2">{designation}</Designation>
+        </CardContent>
+      </SliderCard>
+    </React.Fragment>
+  );
+}
+
+const SliderLeftArrow = withStyles((theme) => ({
+  root: {
+    height: '100%',
+    fontSize: '2.5rem',
+    color: '#18A4E0',
+    cursor: 'pointer',
+  },
+}))(ArrowLeftIcon);
+
+const SliderRightArrow = withStyles((theme) => ({
+  root: {
+    height: '100%',
+    fontSize: '2.5rem',
+    color: '#18A4E0',
+    cursor: 'pointer',
+  },
+}))(ArrowRightIcon);
+
+const ArrowContainer = withStyles((theme) => ({
+  root: {
+    borderRadius: theme.spacing(1),
+    boxShadow: '0 0 5px #b6b6b6',
+  },
+}))(ButtonBase);
+
+const SlideGrid = withStyles((theme) => ({
+  root: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+}))(Grid);
+
+interface IArrowProps {
+  direction: string;
+  clickFunction: () => void;
+}
+
+function Arrow(props: IArrowProps) {
+  const { direction, clickFunction } = props;
+  const icon =
+    direction === 'left' ? <SliderLeftArrow /> : <SliderRightArrow />;
+
+  return <ArrowContainer onClick={clickFunction}>{icon}</ArrowContainer>;
+}
+
+function TestimonySlides() {
+  const slides = [
+    {
+      name: 'Dr. Dilip Kumar',
+      description: `Lizards are a widespread group of squamate reptiles, with over 6,000
+    species, ranging across all continents except Antarctica`,
+      designation: `Anatomy`,
+    },
+    {
+      name: 'Dr. Manoj Kumar',
+      description: `Lizards are a widespread group of squamate reptiles, with over 6,000
+    species, ranging across all continents except Antarctica`,
+      designation: `Pathology`,
+    },
+    {
+      name: 'Dr. Dilip Kumar',
+      description: `Lizards are a widespread group of squamate reptiles, with over 6,000
+    species, ranging across all continents except Antarctica`,
+      designation: `Anatomy`,
+    },
+    {
+      name: 'Dr. Manoj Kumar',
+      description: `Lizards are a widespread group of squamate reptiles, with over 6,000
+    species, ranging across all continents except Antarctica`,
+      designation: `Pathology`,
+    },
+    {
+      name: 'Dr. Dilip Kumar',
+      description: `Lizards are a widespread group of squamate reptiles, with over 6,000
+    species, ranging across all continents except Antarctica`,
+      designation: `Anatomy`,
+    },
+    {
+      name: 'Dr. Manoj Kumar',
+      description: `Lizards are a widespread group of squamate reptiles, with over 6,000
+    species, ranging across all continents except Antarctica`,
+      designation: `Pathology`,
+    },
+  ];
+  const [slideIn, setSlideIn] = React.useState(true);
+  const [index, setIndex] = React.useState(0);
+  const numSlides = slides.length;
+  const [slideDirection, setSlideDirection] = React.useState('right');
+  const onArrowClick = (direction: string) => {
+    const increment = direction === 'left' ? -1 : 1;
+    const newIndex = (index + increment + numSlides) % numSlides;
+    console.log('newIndex', newIndex);
+
+    const oppDirection = direction === 'left' ? 'right' : 'left';
+    setSlideDirection(direction);
+    setSlideIn(false);
+
+    setTimeout(() => {
+      setIndex(newIndex);
+      setSlideDirection(oppDirection);
+      setSlideIn(true);
+    }, 500);
+  };
+  return (
+    <SlideGrid container={true} item={true} xs={12}>
+      <SlideGrid container={true} item={true} xs={1}>
+        <Arrow
+          direction="left"
+          clickFunction={() => {
+            onArrowClick('left');
+          }}
+        />
+      </SlideGrid>
+      <Slide in={slideIn} direction={slideDirection as any}>
+        <SlideGrid container={true} item={true} xs={10}>
+          {slides.slice(index, index + 2).map((item, index) => (
+            <TestimonySlide key={index} item={{ ...item, id: index }} />
+          ))}
+        </SlideGrid>
+      </Slide>
+      <SlideGrid container={true} item={true} xs={1}>
+        <Arrow
+          direction="right"
+          clickFunction={() => {
+            onArrowClick('right');
+          }}
+        />
+      </SlideGrid>
+    </SlideGrid>
   );
 }
 
