c5234dce055a37469ff4b233d375c69522c6ec2d
sravanApt <sravan.apt@gmail.com>
2020-10-22 14:13:10 +0530

    fix: pr comments

--- src/containers/bread-crumbs/Breadcrumbs.tsx
+++ src/containers/bread-crumbs/Breadcrumbs.tsx
@@ -1,33 +1,9 @@
 import React from 'react';
 import Breadcrumbs from '@material-ui/core/Breadcrumbs';
-import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
-import { withStyles, Theme } from '@material-ui/core/styles';
-
-const BreadcrumbLink = withStyles((theme: Theme) => ({
-  root: {
-    fontSize: '1.875rem',
-    color: '#B3C1CD',
-    '&:hover': {
-      textDecoration: 'none',
-    },
-    [theme.breakpoints.down('sm')]: {
-      fontSize: '0.875rem',
-    },
-  },
-}))(Link);
-
-const BreadcrumbText = withStyles((theme: Theme) => ({
-  root: {
-    fontSize: '1.875rem',
-    color: '#2F3D4A',
-    [theme.breakpoints.down('sm')]: {
-      fontSize: '0.875rem',
-    },
-  },
-}))(Typography);
+import BreadcrumbText from 'containers/typography/NavTypography';
+import BreadcrumbLink from 'containers/links/NavLink';
 
 interface Breadcrumb {
   link?: string;
