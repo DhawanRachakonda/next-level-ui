b40f30c23de4a60059a24cd4085a453576b9f2d3
DhawanRachakonda <r.dhawan27@hotmail.co.uk>
2020-10-22 16:21:11 +0530

    test: NetLevelIntl

--- src/lang/NextLevelIntl.test.tsx
+++ src/lang/NextLevelIntl.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { FormattedMessage } from 'react-intl';
 
 import NextLevelIntl from './NextLevelIntl';
+import { render } from 'tests/TestUtils';
 
 describe('test suit for NextLevelIntl', () => {
   it('should generate snapshot', () => {
@@ -9,5 +11,15 @@ describe('test suit for NextLevelIntl', () => {
         <div />
       </NextLevelIntl>,
     );
+  });
+
+  it('should render messages from messages.json', () => {
+    const { getByText } = render(
+      <NextLevelIntl>
+        <FormattedMessage id="nonSecureHeader.login.btn" />
+      </NextLevelIntl>,
+    );
+    const linkElement = getByText(/Login/i);
+    expect(linkElement).toBeInTheDocument();
   });
 });
